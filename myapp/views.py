f625b8c9de1bf62e601ab93b8ab3d4dc58e456bf
Se jeong Kim <somsombenji@naver.com>
2020-12-29 11:10:25 +0900

    second commit

--- myapp/views.py
+++ myapp/views.py
@@ -7,3 +7,8 @@ def home(request):
     #.object : 모델로부터 객체 목록을 전달 받을 수 있게 함.
     #쿼리셋 : 모델로부터 전달 받은 객체 목록
     return render(request, 'home.html', {'blogs':blogs})
+
+    #쿼리셋과 메소드
+    #쿼리셋 메소드 사용 방법
+    # 모델.쿼리셋(.objects).메소드
+    #all: 객체에 있는 모든 쿼리셋(객체)을 가져오는 메소드
